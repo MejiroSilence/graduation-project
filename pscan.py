b6a40597b6d033a5d52a8e1bfa7c9a20e47ed836
MejiroSilence <mejirosilence@gmail.com>
2023-03-23 16:40:59 +0800

    coma actor should not input a as actor code

--- pscan.py
+++ pscan.py
@@ -16,7 +16,7 @@ args:
 """
 class pscan(object):
     def __init__(self,args):
-        self.agents = [gruAgent(args.observeDim+2,args.agentHiddenDim,args.actionNum,args.actorLR) for i in range(args.agentNum)]
+        self.agents = [gruAgent(args.observeDim+1,args.agentHiddenDim,args.actionNum,args.actorLR) for i in range(args.agentNum)]
         self.evalCritic=Qnet(args.agentNum+args.stateDim+args.observeDim+1,args.criticHiddenDim,args.actionNum)
         self.targetCritic=Qnet(args.agentNum+args.stateDim+args.observeDim+1,args.criticHiddenDim,args.actionNum)
         self.criticOptimizer = torch.optim.Adam(self.evalCritic.parameters(), lr=args.criticLR)

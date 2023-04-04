b6f3b888346f828dbe24d195ba5b35e1c61fb2ad
mejirosilence <mejirosilence@gmail.com>
2023-04-04 12:01:43 +0800

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--epsilon', default=0.1)
     parser.add_argument('--gamma', default=0.95)
     parser.add_argument('--tau', default=0.01)
-    parser.add_argument('--epoch', default=1e5)
+    parser.add_argument('--epoch', default=100000)
     parser.add_argument('--evalEp',default=100)
     args=parser.parse_args()
     train(args)
